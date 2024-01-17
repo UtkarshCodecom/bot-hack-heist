daacac64e77b1a3cdbc8bae73bbcd5b9645d9a76
UtkarshCodecom <yushkarsh@gmail.com>
2024-01-17 13:33:04 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,329 @@
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import time
+import os
+import threading
+import json
+import fitz  # Import PyMuPDF for PDF manipulation
+from PIL import Image, ImageDraw, ImageFont
+import os
+
+with open('config.json', 'r') as f: DATA = json.load(f)
+def getenv(var): return os.environ.get(var) or DATA.get(var, None)
+
+bot_token = getenv("TOKEN") 
+api_hash = getenv("HASH") 
+api_id = getenv("ID")
+bot = Client("mybot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+
+ss = getenv("STRING")
+if ss is not None:
+	acc = Client("myacc" ,api_id=api_id, api_hash=api_hash, session_string=ss)
+	acc.start()
+else: acc = None
+
+# download status
+def downstatus(statusfile,message):
+	while True:
+		if os.path.exists(statusfile):
+			break
+
+	time.sleep(3)      
+	while os.path.exists(statusfile):
+		with open(statusfile,"r") as downread:
+			txt = downread.read()
+		try:
+			bot.edit_message_text(message.chat.id, message.id, f"__Downloaded__ : **{txt}**")
+			time.sleep(10)
+		except:
+			time.sleep(5)
+
+
+# upload status
+def upstatus(statusfile,message):
+	while True:
+		if os.path.exists(statusfile):
+			break
+
+	time.sleep(3)      
+	while os.path.exists(statusfile):
+		with open(statusfile,"r") as upread:
+			txt = upread.read()
+		try:
+			bot.edit_message_text(message.chat.id, message.id, f"__Uploaded__ : **{txt}**")
+			time.sleep(10)
+		except:
+			time.sleep(5)
+
+
+# progress writter
+def progress(current, total, message, type):
+	with open(f'{message.id}{type}status.txt',"w") as fileup:
+		fileup.write(f"{current * 100 / total:.1f}%")
+
+
+# start command
+@bot.on_message(filters.command(["start"]))
+def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+	bot.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Save Restricted Bot Programmed by Utkarsh, I can send you restricted content by it's post link__\n\n{USAGE}", reply_to_message_id=None)
+
+
+@bot.on_message(filters.text)
+def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+	print(message.text)
+
+	# joining chats
+	if "https://t.me/+" in message.text or "https://t.me/joinchat/" in message.text:
+
+		if acc is None:
+			bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
+			return
+
+		try:
+			try: acc.join_chat(message.text)
+			except Exception as e: 
+				bot.send_message(message.chat.id,f"**Error** : __{e}__", reply_to_message_id=message.id)
+				return
+			bot.send_message(message.chat.id,"**Chat Joined**", reply_to_message_id=message.id)
+		except UserAlreadyParticipant:
+			bot.send_message(message.chat.id,"**Chat alredy Joined**", reply_to_message_id=message.id)
+		except InviteHashExpired:
+			bot.send_message(message.chat.id,"**Invalid Link**", reply_to_message_id=message.id)
+
+	# getting message
+	elif "https://t.me/" in message.text:
+
+		datas = message.text.split("/")
+		temp = datas[-1].replace("?single","").split("-")
+		fromID = int(temp[0].strip())
+		try: toID = int(temp[1].strip())
+		except: toID = fromID
+
+		for msgid in range(fromID, toID+1):
+
+			# private
+			if "https://t.me/c/" in message.text:
+				chatid = int("-100" + datas[4])
+				
+				if acc is None:
+					bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
+					return
+				
+				handle_private(message,chatid,msgid)
+				# try: handle_private(message,chatid,msgid)
+				# except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__", reply_to_message_id=message.id)
+			
+			# bot
+			elif "https://t.me/b/" in message.text:
+				username = datas[4]
+				
+				if acc is None:
+					bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=message.id)
+					return
+				try: handle_private(message,username,msgid)
+				except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__", reply_to_message_id=message.id)
+
+			# public
+			else:
+				username = datas[3]
+
+				try: msg  = bot.get_messages(username,msgid)
+				except UsernameNotOccupied: 
+					bot.send_message(message.chat.id,f"**The username is not occupied by anyone**", reply_to_message_id=None)
+					return
+
+				try: bot.copy_message(message.chat.id, msg.chat.id, msg.id,reply_to_message_id=None)
+				except:
+					if acc is None:
+						bot.send_message(message.chat.id,f"**String Session is not Set**", reply_to_message_id=None)
+						return
+					try: handle_private(message,username,msgid)
+					except Exception as e: bot.send_message(message.chat.id,f"**Error** : __{e}__", reply_to_message_id=None)
+
+			# wait time
+			time.sleep(3)
+
+
+# handle private
+def handle_private(message: pyrogram.types.messages_and_media.message.Message, chatid: int, msgid: int):
+		msg: pyrogram.types.messages_and_media.message.Message = acc.get_messages(chatid,msgid)
+		msg_type = get_message_type(msg)
+
+		if "Text" == msg_type:
+			bot.send_message(message.chat.id, msg.text, entities=msg.entities, reply_to_message_id=None)
+			return
+
+		smsg = bot.send_message(message.chat.id, '__Downloading__', reply_to_message_id=None)
+		dosta = threading.Thread(target=lambda:downstatus(f'{message.id}downstatus.txt',smsg),daemon=True)
+		dosta.start()
+		file = acc.download_media(msg, progress=progress, progress_args=[message,"down"])
+		os.remove(f'{message.id}downstatus.txt')
+
+		upsta = threading.Thread(target=lambda:upstatus(f'{message.id}upstatus.txt',smsg),daemon=True)
+		upsta.start()
+		
+		if "Document" == msg_type:
+			try:
+				thumb = acc.download_media(msg.document.thumbs[0].file_id)
+			except: thumb = None
+			full_path = file
+			filename = "downloads/" +os.path.basename(full_path)
+			actual_path = os.path.dirname(full_path) + "\ " + os.path.basename(full_path)
+			aplywatermark(filename, file)
+			
+			bot.send_document(message.chat.id, actual_path, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=None, progress=progress, progress_args=[message,"up"])
+			if thumb != None: os.remove(thumb)
+			os.remove(actual_path)
+		elif "Video" == msg_type:
+			try: 
+				thumb = acc.download_media(msg.video.thumbs[0].file_id)
+			except: thumb = None
+
+			bot.send_video(message.chat.id, file, duration=msg.video.duration, width=msg.video.width, height=msg.video.height, thumb=thumb, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=None, progress=progress, progress_args=[message,"up"])
+			if thumb != None: os.remove(thumb)
+
+		elif "Animation" == msg_type:
+			bot.send_animation(message.chat.id, file, reply_to_message_id=None)
+			   
+		elif "Sticker" == msg_type:
+			bot.send_sticker(message.chat.id, file, reply_to_message_id=None)
+
+		elif "Voice" == msg_type:
+			bot.send_voice(message.chat.id, file, caption=msg.caption, thumb=thumb, caption_entities=msg.caption_entities, reply_to_message_id=None, progress=progress, progress_args=[message,"up"])
+
+		elif "Audio" == msg_type:
+			try:
+				thumb = acc.download_media(msg.audio.thumbs[0].file_id)
+			except: thumb = None
+				
+			bot.send_audio(message.chat.id, file, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=None, progress=progress, progress_args=[message,"up"])   
+			if thumb != None: os.remove(thumb)
+
+		elif "Photo" == msg_type:
+			bot.send_photo(message.chat.id, file, caption=msg.caption, caption_entities=msg.caption_entities, reply_to_message_id=None)
+
+		os.remove(file)
+		if os.path.exists(f'{message.id}upstatus.txt'): os.remove(f'{message.id}upstatus.txt')
+		bot.delete_messages(message.chat.id,[smsg.id])
+
+
+def aplywatermark(filename, file):
+    doc = fitz.open(filename)
+    output_doc = fitz.open()
+    for page_index in range(len(doc)):
+        page = doc[page_index]
+        pix = page.get_pixmap()
+        image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+        crop_box = (
+	        image.width - (image.width // 1),  # Adjust coordinates as needed
+	        image.height - (image.height // 1.05),
+	        image.width,
+	        image.height
+	    )
+        watermark_free_image = image.crop(crop_box)
+        watermark_free_image.save(f"watermark_free_page_{page_index + 1}.png")
+        image = Image.open(f"watermark_free_page_{page_index + 1}.png")
+        width, height = image.size
+        draw = ImageDraw.Draw(image)
+        text = 't.me/bookskakhazana'
+        font_size = 36  # Adjust as needed
+        font = ImageFont.truetype("CabalBold-78yP.ttf", font_size)
+        textwidth, textheight = draw.textsize(text, font=font)
+        margin = 60
+        x = 20
+        y = height - textheight - margin
+        draw.text((x, y), text, font=font)
+        image.save(f"watermark_free_page_{page_index + 1}.png")
+        output_page = output_doc.new_page(width=page.rect.width, height=page.rect.height)
+        output_page.insert_image(
+	        filename=f"watermark_free_page_{page_index + 1}.png", rect=page.rect
+	    )
+    full_pah = file
+    filenae = "downloads/ " +os.path.basename(full_pah)
+    output_doc.save(filenae)
+    for page_index in range(len(doc)):
+        os.remove(f"watermark_free_page_{page_index + 1}.png")  # Delete the image
+    doc.close()
+    output_doc.close()
+# get the type of message
+def get_message_type(msg: pyrogram.types.messages_and_media.message.Message):
+	try:
+		msg.document.file_id
+		return "Document"
+	except: pass
+
+	try:
+		msg.video.file_id
+		return "Video"
+	except: pass
+
+	try:
+		msg.animation.file_id
+		return "Animation"
+	except: pass
+
+	try:
+		msg.sticker.file_id
+		return "Sticker"
+	except: pass
+
+	try:
+		msg.voice.file_id
+		return "Voice"
+	except: pass
+
+	try:
+		msg.audio.file_id
+		return "Audio"
+	except: pass
+
+	try:
+		msg.photo.file_id
+		return "Photo"
+	except: pass
+
+	try:
+		msg.text
+		return "Text"
+	except: pass
+ 
+
+
+
+
+USAGE = """**FOR PUBLIC CHATS**
+
+__just send post/s link__
+
+**FOR PRIVATE CHATS**
+
+__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link__
+
+**FOR BOT CHATS**
+
+__send link with '/b/', bot's username and message id, you might want to install some unofficial client to get the id like below__
+
+```
+https://t.me/b/botusername/4321
+```
+
+**MULTI POSTS**
+
+__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
+
+```
+https://t.me/xxxx/1001-1010
+
+https://t.me/c/xxxx/101 - 120
+```
+
+__note that space in between doesn't matter__
+"""
+
+
+# infinty polling
+bot.run()
